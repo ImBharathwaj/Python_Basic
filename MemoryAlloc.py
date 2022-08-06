8002be621b6f59431dd0e49c4eb8a9344c771d7c
Bharathwaj Sivakumar <bharathwajist@gmail.com>
2022-08-06 10:01:43 +0000

    Memory Allocation of Python Program

--- MemoryAlloc.py
+++ MemoryAlloc.py
@@ -2,5 +2,7 @@ import os
 import psutil
 
 process = psutil.Process(os.getpid())
-resident_mem = process.memory_info().rss    # Resident Set Size
-virtual_mem = process.memory_info().vms     # Virtual memory size
+resident_mem = process.memory_info().rss/1024/1024    # Resident Set Size
+virtual_mem = process.memory_info().vms/1024/1024     # Virtual memory size
+print('Resident Memory : ', resident_mem, 'MBs')
+print('Resident Memory : ', virtual_mem, 'MBs')
