adfcce29af9f558a198b56725e6b09a04f67f0ea
Bharathwaj Sivakumar <bharathwajist@gmail.com>
2022-09-05 06:49:39 +0530

    Graphical User Interface with Python

---
+++ GUI.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("New Tool")
+root.geometry('350x200')
+
+menu = Menu(root)
+item = Menu(menu)
+item.add_command(label='New')
+menu.add_cascade(label='File', menu=item)
+root.config(menu=menu)
+
+lbl = Label(root, text = 'Are you a Sekar ?')
+lbl.grid()
+
+txt = Entry(root, width=10)
+txt.grid(column=1, row=0)
+
+def clicked():
+    res = 'You Wrote' + txt.get()
+    lbl.configure(text = res)
+
+btn = Button(root, text='Click me',
+        fg = 'red', command=clicked)
+
+btn.grid(column=2, row=0)
+
+root.mainloop()
